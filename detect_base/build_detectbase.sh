77fe3b944916ae37c2dda8f8399dac6692075779
heabeounMKTO <heabeoun69zin@gmail.com>
2023-07-04 11:53:27 +0700

    build cache scripts

--- detect_base/build_detectbase.sh
+++ detect_base/build_detectbase.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-docker build -t localhost:5000/detectbase . 
+while getopts c: flag
+do 
+  case "${flag}" in 
+    c) cache=${OPTARG};;
+  esac
+done
+
+if [ "$cache" = "true" ]
+then 
+  echo "building detectbase with cache"
+  docker build -t localhost:5000/detectbase . --build-arg CACHE=true 
+else
+  echo "building detectbase without cache"
+  docker build -t localhost:5000/detectbase:cache .
+fi
